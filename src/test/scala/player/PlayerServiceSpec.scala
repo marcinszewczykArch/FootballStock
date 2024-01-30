0ad20d1e2c6a54f3623336444e2da754682fd294
marcin.szewczyk <marcin.szewczyk@ocado.com>
2024-01-30 11:28:20 +0100

    add playerService tests

--- src/test/scala/player/PlayerServiceSpec.scala
+++ src/test/scala/player/PlayerServiceSpec.scala
@@ -3,21 +3,29 @@ package player
 import cats.effect.IO
 import cats.effect.Ref
 import config.AppConfig
+import game.player.client.domain.FetchedPlayerProfile
+import game.player.client.domain.PlayerSearchResponse
 import game.player.client.memory.PlayerProfileClientMemory
+import game.player.service.PlayerMapper
+import game.player.service.PlayerMapper.fetchedPlayerSimpleToPlayerSimple
 import game.player.service.PlayerService
 import game.player.service.domain.PlayerId
+import game.player.service.domain.PlayerProfile
+import game.state.domain.UserGameState
 import io.circe.Json
+import io.circe.parser
 import munit.CatsEffectSuite
 import org.typelevel.log4cats.LoggerFactory
 import org.typelevel.log4cats.SelfAwareStructuredLogger
 import org.typelevel.log4cats.slf4j.Slf4jFactory
 import testUtils._
+import utils.JsonParser
 
 class PlayerServiceSpec extends CatsEffectSuite {
   private implicit val testLoggerFactory: LoggerFactory[IO] = Slf4jFactory.create[IO]
   implicit val log: SelfAwareStructuredLogger[IO]           = LoggerFactory.getLoggerFromName[IO](classOf[PlayerServiceSpec].getName)
 
-  def getTestPlayerService(playerProfileClientMemoryRef: Ref[IO, Map[PlayerId, Json]]): IO[PlayerService[IO]] = for {
+  private def getTestPlayerService(playerProfileClientMemoryRef: Ref[IO, Map[PlayerId, Json]]): IO[PlayerService[IO]] = for {
     testRawAppConfig                        <- AppConfig.getTypesafeConfig[IO]
     appConfig                               <- AppConfig.parseAppConfig[IO](testRawAppConfig)
     _                                       <- log.info(s"Test config loaded: $appConfig")
@@ -32,16 +40,29 @@ class PlayerServiceSpec extends CatsEffectSuite {
     playerService                           <- IO.delay(PlayerService.impl[IO](playerProfileClientMemoryCached, testPlayerSearchClient))
   } yield playerService
 
+  private def expectedPlayerFromPath(path: String): PlayerProfile = {
+    val playerString          = JsonParser.jsonString(path)
+    val playerJson            = parser.parse(playerString).toOption.get
+    val expectedPlayerProfile = PlayerMapper.fetchedPlayerProfileToProfile(playerJson.as[FetchedPlayerProfile].toOption.get)
+    expectedPlayerProfile
+  }
+
   test("getPlayerProfileById spec") {
     for {
       playerProfileClientMemoryRef <- Ref.of[IO, Map[PlayerId, Json]](Map.empty[PlayerId, Json])
       playerService                <- getTestPlayerService(playerProfileClientMemoryRef)
 
-//      fetchedJson <- playerProfileClient.fetchRawPlayerProfileById(PlayerId(38253))
-//      expectedJsonStr = jsonString("testResponsePlayerProfile.json")
-//      expectedJson = parse(expectedJsonStr).toOption.get
-//
-//      _ = assertEquals(fetchedJson.toOption.get, expectedJson)
+      fetchedPlayer38253 <- playerService.getPlayerProfileById(PlayerId(38253))
+      expectedPlayer38253 = expectedPlayerFromPath("players/38253.json")
+      _                   = assertEquals(fetchedPlayer38253.toOption.get, expectedPlayer38253)
+
+      fetchedPlayer38254 <- playerService.getPlayerProfileById(PlayerId(38254))
+      expectedPlayer38254 = expectedPlayerFromPath("players/38254.json")
+      _                   = assertEquals(fetchedPlayer38254.toOption.get, expectedPlayer38254)
+
+      fetchedPlayer38255 <- playerService.getPlayerProfileById(PlayerId(38255))
+      expectedPlayer38255 = expectedPlayerFromPath("players/38255.json")
+      _                   = assertEquals(fetchedPlayer38255.toOption.get, expectedPlayer38255)
     } yield ()
   }
 
@@ -50,11 +71,10 @@ class PlayerServiceSpec extends CatsEffectSuite {
       playerProfileClientMemoryRef <- Ref.of[IO, Map[PlayerId, Json]](Map.empty[PlayerId, Json])
       playerService                <- getTestPlayerService(playerProfileClientMemoryRef)
 
-      //      fetchedJson <- playerProfileClient.fetchRawPlayerProfileById(PlayerId(38253))
-      //      expectedJsonStr = jsonString("testResponsePlayerProfile.json")
-      //      expectedJson = parse(expectedJsonStr).toOption.get
-      //
-      //      _ = assertEquals(fetchedJson.toOption.get, expectedJson)
+      fetchedMarketValue38253 <- playerService.getMarketValueByPlayerId(PlayerId(38253))
+      expectedMarketValue38253 = expectedPlayerFromPath("players/38253.json").marketValue
+      _                        = assertEquals(fetchedMarketValue38253.toOption.get, expectedMarketValue38253)
+
     } yield ()
   }
 
@@ -63,11 +83,12 @@ class PlayerServiceSpec extends CatsEffectSuite {
       playerProfileClientMemoryRef <- Ref.of[IO, Map[PlayerId, Json]](Map.empty[PlayerId, Json])
       playerService                <- getTestPlayerService(playerProfileClientMemoryRef)
 
-      //      fetchedJson <- playerProfileClient.fetchRawPlayerProfileById(PlayerId(38253))
-      //      expectedJsonStr = jsonString("testResponsePlayerProfile.json")
-      //      expectedJson = parse(expectedJsonStr).toOption.get
-      //
-      //      _ = assertEquals(fetchedJson.toOption.get, expectedJson)
+      fetchedResult <- playerService.searchByName("anyInputShouldWork")
+      expectedString = JsonParser.jsonString("playerSearch/testResponsePlayerSearch.json")
+      expectedJson   = parser.parse(expectedString).toOption.get
+      expectedResult = expectedJson.as[PlayerSearchResponse].toOption.get.result.map(fetchedPlayerSimpleToPlayerSimple)
+
+      _ = assertEquals(fetchedResult.toOption.get, expectedResult)
     } yield ()
   }
 
